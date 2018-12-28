d207ff060f0f358f4655ca935eec2fbf60f0e6d6
barnsoir <barney.lewis@gmail.com>
2018-12-28 19:45:57 +0000

    Initial commit for Pong game

---
+++ app/src/main/java/com/nervouscow/pong/PongActivity.java
@@ -0,0 +1,12 @@
+package com.nervouscow.pong;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PongActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/nervouscow/pong/PongGame.java
@@ -0,0 +1,4 @@
+package com.nervouscow.pong;
+
+class PongGame {
+}
