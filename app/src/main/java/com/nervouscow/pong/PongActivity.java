cc539d8db3e27bdc608773b2d5b19af81f66b770
barnsoir <barney.lewis@gmail.com>
2018-12-29 13:14:32 +0000

    Chapter 9 changes - game engine, threads and game loop

--- app/src/main/java/com/nervouscow/pong/PongActivity.java
+++ app/src/main/java/com/nervouscow/pong/PongActivity.java
@@ -1,12 +1,39 @@
 package com.nervouscow.pong;
 
 import android.app.Activity;
+import android.graphics.Point;
 import android.os.Bundle;
+import android.view.Display;
 
 public class PongActivity extends Activity {
+
+    private PongGame mPongGame;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+
+        mPongGame = new PongGame(this, size.x, size.y);
+        setContentView(mPongGame);
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+
+        // More code here later
+        mPongGame.resume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // More code here later
+        mPongGame.pause();
     }
 }
